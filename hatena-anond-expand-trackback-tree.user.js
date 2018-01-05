0b97f451dadf0a93bc6537ac82b3832b2551c4a8
Kohei Hasegawa <ameutau@gmail.com>
2018-01-06 03:51:04 +0900

    Get back very old code from userscripts.org
    Originally it was here: http://userscripts-mirror.org/scripts/show/48681
    
    * Updated codes to work in Chrome and Firefox
      * Adapted with Greasemonkey 4.0
    * Updated codes to be modern

---
+++ hatena-anond-expand-trackback-tree.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name           Hatena Anond Expand Trackback Tree
+// @namespace      https://github.com/banyan/
+// @version        1.0
+// @description    はてな匿名ダイアリーのトラックバックツリーをはじめから開いた状態にする
+// @match          https://anond.hatelabo.jp/*
+// @require        https://gist.githubusercontent.com/os0x/29681/raw/2cedf778cedec6ca4f70b753172fbab445dd0c47/dollarX.js
+// ==/UserScript==
+
+(() => {
+  const executeBrowserContext = (ids) => {
+    ids.forEach((id) => {
+      window.eval(`toggleTBContent(${id})`) // works with GM4 as well
+    })
+  }
+
+  const links = $X("//div[@class=\"refererlist\"]/descendant::li/descendant::a[1]")
+  const ids = links.filter(link => (
+    link.className !== 'self' && link.href.match(/^https:\/\/anond\.hatelabo\.jp/) // 今開いているエントリーのトラックバックは開かない
+  )).map(link => link.href.match(/\d+/))
+
+  executeBrowserContext(ids)
+})()
