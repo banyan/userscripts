f39112d75048f6b268ce9955095f824a195ea77e
Kohei Hasegawa <ameutau@gmail.com>
2020-04-20 23:46:58 +0900

    Handle dynamic loading

--- github-pull-request-normalize-path-for-monorepo.user.js
+++ github-pull-request-normalize-path-for-monorepo.user.js
@@ -7,6 +7,14 @@
 // ==/UserScript==
 
 (() => {
+  function debounce(fn, waitMs) {
+    let timeout;
+    return function (...args) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => fn.call(this, ...args), waitMs);
+    };
+  }
+
   const normalizePathForMonorepo = () => {
     document.querySelectorAll('clipboard-copy').forEach((clipboard) => {
       const regexp = /^packages\/(.*)\/src\/(.*)$/;
@@ -17,5 +25,28 @@
     });
   };
 
-  window.addEventListener('load', normalizePathForMonorepo);
+  const observeContainer = () => {
+    Array.from(
+      document.getElementsByClassName('js-diff-progressive-container'),
+    ).forEach((target) => {
+      new MutationObserver(
+        debounce(normalizePathForMonorepo, 1000),
+      ).observe(target, { childList: true });
+    });
+  };
+
+  window.addEventListener('load', () => {
+    normalizePathForMonorepo();
+    observeContainer();
+  });
+
+  // Listen page transition event by pjax
+  document.addEventListener(
+    'pjax:end',
+    () => {
+      normalizePathForMonorepo();
+      observeContainer();
+    },
+    true,
+  );
 })();
